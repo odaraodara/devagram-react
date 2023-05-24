782863863bf88fb09e931fddb72ac7f9605e874a
Odara Valença <odaravalenca@gmail.com>
2023-05-24 17:25:29 -0300

    componente - avatar

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Botao from '@/componentes/botao'
+import Avatar from '../componentes/avatar'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <h1>Olá Mundo!</h1>
-      <div style={{width:200}}>
-      <Botao texto={'Loguin'}  manipularClick={() => console.log('botão clicado')} />
+      <div style={{ width: 200 }}>
+        <Avatar />
+        <Botao texto={'Loguin'} manipularClick={() => console.log('botão clicado')} />
       </div>
     </>
   )
