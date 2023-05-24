2c8caee2cd4f79334843ffbe0a04c7a4a67021ea
Odara Valença <odaravalenca@gmail.com>
2023-05-24 17:02:52 -0300

    componente - botão

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Botao from '@/componentes/botao'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <div style={{width:200}}>
+      <Botao texto={'Loguin'}  manipularClick={() => console.log('botão clicado')} />
+      </div>
+    </>
   )
 }
